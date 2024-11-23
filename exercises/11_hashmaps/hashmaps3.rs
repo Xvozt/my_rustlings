b6c6c96b5770d0042cce82de429d382883969ccc
s.v.kupriyanov <s.v.kupriyanov@tinkoff.ru>
2024-11-23 23:37:22 +0400

    Finished 10_modules and 11_hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -17,7 +17,7 @@ struct TeamScores {
 
 fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
     // The name of the team is the key and its associated struct is the value.
-    let mut scores = HashMap::new();
+    let mut scores: HashMap<&str, TeamScores> = HashMap::new();
 
     for line in results.lines() {
         let mut split_iterator = line.split(',');
@@ -31,10 +31,31 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        // England,4
+        // France,2
+        // France,3
+        // Italy,1
+        // Poland,2
+        // Spain,0
+        // Germany,2
+        // England,1
+        // England,1
+        // Spain,0
+        let team_1 = scores.entry(team_1_name).or_default();
+        team_1.goals_scored += team_1_score;
+        team_1.goals_conceded += team_2_score;
+        let team_2 = scores.entry(team_2_name).or_default();
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
     }
 
     scores
 }
+
+// идем по результатам, получаем первую команду, её голы, вторую команду и её голы
+// берем первуб команду, проверяем в мапе, если этот ключ уже есть, то:
+// берем текущее значение гола, прибавляем к полученному из списка, вставляеем в мапу по этому ключу
+// если нет, просто вставляем голы
 
 fn main() {
     // You can optionally experiment here.
